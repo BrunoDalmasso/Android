38817a85523c135bbefb87e2aef8d3d757ae487c
BrunoDalmasso <bruno.dalmasso@hotmail.com>
2019-02-15 20:32:31 -0200

    Aula 02
    Criacao de um projeto que altera a imagem clicando no botão

--- Aula_02/app/src/main/java/br/com/br/aula02manipulandoimagem/MainActivity.java
+++ Aula_02/app/src/main/java/br/com/br/aula02manipulandoimagem/MainActivity.java
@@ -2,12 +2,29 @@ package br.com.br.aula02manipulandoimagem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPessoa = findViewById(R.id.imgPessoa);
+    }
+
+    public void pessoa01(View view) {
+        imgPessoa.setImageResource(R.drawable.p1);
+    }
+
+    public void pessoa02(View view) {
+        imgPessoa.setImageResource(R.drawable.p2);
+    }
+
+    public void pessoa03(View view) {
+        imgPessoa.setImageResource(R.drawable.p3);
     }
 }
