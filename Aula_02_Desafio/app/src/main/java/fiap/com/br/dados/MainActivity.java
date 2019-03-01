e8969a986f026fb682fce0ea15395459edd6c768
BrunoDalmasso <bruno.dalmasso@hotmail.com>
2019-03-01 20:18:39 -0300

    Aula_03 Refatoracao
    Refatoração do codigo para sei la pq

--- Aula_02_Desafio/app/src/main/java/fiap/com/br/dados/MainActivity.java
+++ Aula_02_Desafio/app/src/main/java/fiap/com/br/dados/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -12,65 +13,38 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgDado1;
     private ImageView imgDado2;
 
+    private int[] imagens = {
+            R.drawable.d1,
+            R.drawable.d2,
+            R.drawable.d3,
+            R.drawable.d4,
+            R.drawable.d5,
+            R.drawable.d6
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgDado1 = findViewById(R.id.imgDado1);
-        imgDado2 = findViewById(R.id.imgDado2);
+        this.imgDado1 = findViewById(R.id.imgDado1);
+        this.imgDado2 = findViewById(R.id.imgDado2);
     }
 
     public void rolarDados(View view) {
-        switch (gerador.nextInt(6)) {
-            case 1:
-                imgDado1.setImageResource(R.drawable.d1);
-                break;
-            case 2:
-                imgDado1.setImageResource(R.drawable.d2);
-                break;
-            case 3:
-                imgDado1.setImageResource(R.drawable.d3);
-                break;
-            case 4:
-                imgDado1.setImageResource(R.drawable.d4);
-                break;
-            case 5:
-                imgDado1.setImageResource(R.drawable.d5);
-                break;
-            default:
-                imgDado1.setImageResource(R.drawable.d6);
-                break;
+        int jogada1 = new Random().nextInt(imagens.length);
+        int jogada2 = new Random().nextInt(imagens.length);
+
+        this.imgDado1.setImageResource(imagens[jogada1]);
+        this.imgDado2.setImageResource(imagens[jogada2]);
+
+        if (jogada1 == jogada2) {
+            Toast.makeText(this, "Ocorreu um epate!!!", Toast.LENGTH_LONG).show();
+        } else if (jogada1 > jogada2) {
+            Toast.makeText(this, "O jagador 1 ganhou!!!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "O jagador 2 ganhou!!!", Toast.LENGTH_LONG).show();
         }
-
-        switch (gerador.nextInt(6)) {
-            case 1:
-                imgDado2.setImageResource(R.drawable.d1);
-                break;
-            case 2:
-                imgDado2.setImageResource(R.drawable.d2);
-                break;
-            case 3:
-                imgDado2.setImageResource(R.drawable.d3);
-                break;
-            case 4:
-                imgDado2.setImageResource(R.drawable.d4);
-                break;
-            case 5:
-                imgDado2.setImageResource(R.drawable.d5);
-                break;
-            default:
-                imgDado2.setImageResource(R.drawable.d6);
-                break;
-        }
-    }
-
-    private int rolagemJogador1() {
-        return gerador.nextInt(6);
-    }
-
-    private int rolagemJogador2() {
-        return gerador.nextInt(6);
     }
 
 }
